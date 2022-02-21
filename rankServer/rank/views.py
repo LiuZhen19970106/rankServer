d612f97ed684d6565038a777c7a8dee718772795
LiuZhen19970106 <851091368@qq.com>
2022-02-21 12:31:57 +0800

    refactor: update return format

--- rankServer/rank/views.py
+++ rankServer/rank/views.py
@@ -1,8 +1,25 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.db.models.functions import Rank
 from django.db.models import Window, F
 from django.shortcuts import render
 from rank.models import Score
+
+class ErrorMessage():
+    UPLOAD_SCORE_ERROR = 'score must be number and between 1 and 10000000'
+    UPLOAD_CLENT_ERROR = 'client_name is required'
+    UPLOAD_MISS_SCORE_ERROR = 'score is required'
+    SEARCH_FILLTER_ERROR = 'start and end must be number '
+    SEARCH_CLIENT_ERROR = 'client_name is required'
+    SEARCH_INVALID_START_END = 'start must greater than or equal to 1 or start must be less than end'
+    SEARCH_MISS_CLIENT_ERROR = 'client_name is not exist'
+    SEARCH_SCORE_ERROR = 'there is no data in database'
+
+def generate_res(status_code, message, data):
+    return {
+        'status_code': status_code,
+        'msg': message,
+        'data': data
+    }
 
 # Create your views here.
 def upload(request):
@@ -11,10 +28,11 @@ def upload(request):
     try:
         score = float(request.GET.get('score', ''))
     except:
-        return HttpResponse('score must be number and between 1 and 10000000')
+        
+        return JsonResponse(generate_res(400, 'failed', ErrorMessage.UPLOAD_SCORE_ERROR))
     if client_name and score:
         if score<1 or score>10000000:
-            return HttpResponse('score must be between 1 and 10000000')
+            return JsonResponse(generate_res(400, 'failed', ErrorMessage.UPLOAD_SCORE_ERROR))
         old_scor = Score.objects.filter(client=client_name).first()
         if old_scor:
             if old_scor.score != score:
@@ -22,29 +40,33 @@ def upload(request):
                 old_scor.save()
         else:
             Score.objects.create(client=client_name, score=score)
-        return HttpResponse('upload success')
+        return JsonResponse(generate_res(200, 'success', 'upload success'))
     elif not client_name:
-        return HttpResponse('client_name is required')
+        return JsonResponse(generate_res(400, 'failed', ErrorMessage.UPLOAD_CLENT_ERROR))
     elif not score:
-        return HttpResponse('score is required')
+        return JsonResponse(generate_res(400, 'failed', ErrorMessage.UPLOAD_MISS_SCORE_ERROR))
 
 
 def search(request):
     client_name = request.GET.get('client_name', '')
     start = request.GET.get('start', 1)
     end = request.GET.get('end', Score.objects.all().count())
-
+    
     try:
         start = int(start)
         end = int(end)
+        if end>Score.objects.all().count():
+            end = Score.objects.all().count()
     except:
-        return HttpResponse('start and end must be integer')
-    
+        return JsonResponse(generate_res(400, 'failed', ErrorMessage.SEARCH_FILLTER_ERROR))
+        
     if client_name:
-        if start<1 or start>end or end>1000000:
-            return HttpResponse('start and end must be between 1 and 1000000')
+        if start<1 or start>end:
+            return JsonResponse(generate_res(400, 'failed', ErrorMessage.SEARCH_INVALID_START_END))
+            
         if not Score.objects.filter(client=client_name).first():
-            return HttpResponse('client_name has no score in database')
+            return JsonResponse(generate_res(400, 'failed', ErrorMessage.SEARCH_MISS_CLIENT_ERROR))
+            
         # 获取排名
         get_rank = Score.objects.all().annotate(score_rank=Window(expression=Rank(), order_by=F('score').desc())).values('score_rank', 'client', 'score')
         user_rank = list(filter(lambda x: x['client'] == client_name, get_rank))[0]
@@ -53,9 +75,10 @@ def search(request):
                               get_rank[start - 1:end]], 'userInfo': userInfo}
 
         if len(context['scores'])<start:
-            return HttpResponse('there is no rank in database')
+            return JsonResponse(generate_res(400, 'failed', ErrorMessage.SEARCH_SCORE_ERROR)) 
         else:
             return render(request, 'result.html', {'context': context})
     elif not client_name:
-        return HttpResponse('client_name is required')
+        return JsonResponse(generate_res(400, 'failed', ErrorMessage.SEARCH_CLIENT_ERROR))
+        
 
